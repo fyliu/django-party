2689188822d06b7e11fe5c2fb5056fbec8d28ded
Fang Yi Liu <fangyiliu@gmail.com>
2025-03-14 23:50:19 -0700

    add admin classes

--- party/admin.py
+++ party/admin.py
@@ -1,1 +1,27 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .models import Gift
+from .models import Guest
+from .models import Party
+
+
+@admin.register(CustomUser)
+class UserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
+
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
