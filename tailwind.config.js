78a981bebbaf69e1faf4b932d086cacf9ac01b34
Fang Yi Liu <fangyiliu@gmail.com>
2025-03-17 23:21:32 -0700

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,13 @@ module.exports = {
         "roboto": ["Roboto", "sans-serif"], // making possible to use classes font-roboto and font-dancing-script
         "dancing-script": ["Dancing Script", "cursive"],
       },
+      colors: {
+        "custom-powder-light": "#eee2dc",
+        "custom-powder": "#edc7b7",
+        "custom-blue": "#123c69",
+        "custom-blue-light": "#3a6aa4",
+        "custom-red": "#ac3b61",
+      },
     },
   },
   plugins: [],
