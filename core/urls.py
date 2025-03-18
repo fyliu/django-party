8552843fef672ef67ad556bd7c3c84a3e5bd5d24
Fang Yi Liu <fangyiliu@gmail.com>
2025-03-17 23:07:36 -0700

    config django-browser-reload

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
