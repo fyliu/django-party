73c6fd053baabc578f036fe7dbe30754d601bf3c
Fang Yi Liu <fangyiliu@gmail.com>
2025-03-21 16:34:53 -0700

    create party list page

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,5 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("", include("party.urls")),
 ]
