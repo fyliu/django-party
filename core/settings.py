e8a2d06150733f6dd6082bed095d4c30152fd2e3
Fang Yi Liu <fangyiliu@gmail.com>
2025-03-21 21:22:56 -0700

    add django-crispy-forms
    uv add django-crispy-forms crispy-bootstrap4
    uv pip compile -o requirements.txt pyproject.toml

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_browser_reload",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "party",
 ]
 
@@ -126,3 +128,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "party.CustomUser"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
