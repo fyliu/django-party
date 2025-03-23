e58e950ae770b87857e9ebb289a3d3796ab00c8c
Fang Yi Liu <fangyiliu@gmail.com>
2025-03-23 02:09:38 -0700

    add party detail page with click to edit form

--- party/views/__init__.py
+++ party/views/__init__.py
@@ -1,5 +1,5 @@
+from .party_details_views import PartyDetailPage
+from .party_details_views import PartyDetailPartial
 from .party_list_views import PartyListPage
 
-__all__ = [
-    "PartyListPage",
-]
+__all__ = ["PartyDetailPage", "PartyDetailPartial", "PartyListPage"]
