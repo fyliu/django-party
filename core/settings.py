4d508ffc6c23d4c3c310fbe000a3b7f4d46fab81
Fang Yi Liu <fangyiliu@gmail.com>
2025-03-26 02:52:45 -0700

    set allowed_hosts to all

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_601&#(_-h&qj1f=4tprtwh4uuo)(2de&6+0n5hkfjcgr!-2iw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
